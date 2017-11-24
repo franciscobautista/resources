9a115ffcfe0c4740cec4cb09d503a02f2c181e6b
Francisco Mario Bautista Hernández <fmariobh@gmail.com>
2017-11-24 12:38:40 -0600

    Views: user, resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: "home#index"
+  get '/user', to: "users#profile"
+  get '/user/edit', to: "users#edit"
+
+  get '/upload', to: "resources#upload"
+  get '/resource', to: "resources#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
